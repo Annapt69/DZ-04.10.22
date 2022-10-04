20cca263b0abac39287bc5b2f049d786acf23fa6
«Ann» <annapt6969@gmail.com>
2022-10-04 11:06:59 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+int[] getRandomArray(int length, int startPoint, int endPoint)
+{
+    int[] resultArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = new Random().Next(startPoint, endPoint + 1);
+    }
+    return resultArray;
+}
+void printArray(int[] incomingArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        Console.Write(incomingArray[i]);
+        if (i < incomingArray.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int getNumbersOfEvenNumber(int[] incomingArray)
+{
+    int count = 0;
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        if(incomingArray[i] % 2 == 0)
+        {
+            count = count + 1;
+        }  
+    }
+    return count;
+}
+
+
+int[] currentArray = getRandomArray(19, 100, 999);
+printArray(currentArray);
+int NumbersOfEvenNumber = getNumbersOfEvenNumber(currentArray);
+Console.WriteLine($"Количество четных чисел в массиве {NumbersOfEvenNumber} ");
