c2ba62c1301acbbc016836e8f51a09a07463fcc9
«Ann» <annapt6969@gmail.com>
2022-10-04 16:17:08 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76
+*/
+
+double[] getDoubleArray(int length, double startPoint, double endPoint)
+{
+    double[] resultArray = new double[length];
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = Math.Round((new Random().NextDouble() * 100), 2);
+    }
+    return resultArray;
+}
+void printArray(double[] incomingArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        Console.Write(incomingArray[i]);
+        if (i < incomingArray.Length - 1)
+        {
+            Console.Write(";");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+double getDifferenceMaxOfMin(double[] incomingArray)
+{
+    double max = 0;
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        if (incomingArray[i] > max) max = incomingArray[i];
+        {
+            incomingArray[i] = incomingArray[i] + 1;
+        }
+    
+        double min = 0;
+        for (int j = i + 1; j < incomingArray.Length; j++)
+        {
+            if (incomingArray[j] < min) min = incomingArray[j];
+            {
+                incomingArray[j] = incomingArray[j] + 1;
+            }
+        }
+    }
+double result = 0;  
+if (max > 0 && min > 0)
+{   
+    result = max - min;
+}
+return result;
+}
+
+double[] currentArray = getDoubleArray(5, 1, 99);
+printArray(currentArray);
+
+double DifferenceMaxOfMin = getDifferenceMaxOfMin(currentArray);
+Console.WriteLine($"Разница между максимальным и минимальным элементов массива {DifferenceMaxOfMin} ");
